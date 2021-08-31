9a70bd4964380076927adab8e0d56de709f0d536
Hanley Lee <hanley.lei@gmail.com>
2021-09-01 03:21:08 +0800

    [fix]fix st not change on time level

--- st.py
+++ st.py
@@ -12,18 +12,18 @@ from ts_st_utils import *
 def main(wf):
     log.debug('Started')
 
-    args = wf.args[0]
-    args = args.split(' ')
+    # args = wf.args[0]
+    # args = args.split(' ')
     if len(wf.args) == 0:
         output_for_default(wf)
     else:
-        arg0 = args[0]
+        arg0 = wf.args[0]
         try:
             dt = parse(arg0)
-        except BaseException:
-            dt = now_ts()
-    ts = time.mktime(dt.timetuple())
-    output(ts, wf)
+        except:
+            dt = now()
+        ts = time.mktime(dt.timetuple())
+        output(ts, wf)
 
 
 if __name__ == "__main__":
